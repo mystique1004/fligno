bf3c07f234d32e19723e346b8cffa9ae98817eca
mystique1004 <lsiguis@yahoo.com>
2022-07-27 18:22:06 -0400

    bootstrap and controller update

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('demo');
 });
+
+Route::get('my-home', 'HomeController@myHome');
+
+Route::get('my-users', 'HomeController@myUsers');
